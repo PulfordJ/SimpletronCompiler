52403b6e98ef163836657175b5e7eda89c9c3fa8
John Pulford <johnpulford@gmail.com>
2013-02-24 20:29:41 +0000

    Made Simpletron OOP.

--- Simpletron.cpp
+++ Simpletron.cpp
@@ -6,9 +6,129 @@
  */
 
 #include "Simpletron.h"
+#include <iostream>
+#include <iomanip>
 
-Simpletron::Simpletron() {
-	// TODO Auto-generated constructor stub
+using namespace std;
 
+Simpletron::Simpletron() :
+		//Initialise special registers.
+		instructionRegister(0), operationCode(0), operand(0), action(0), accumulator(
+				0), instructionCounter(0) {
+	for (int i = 0; i < MEMSIZE; i++) {
+		memory[i] = 0;
+	}
+
+}
+
+void Simpletron::inputInstructions() {
+
+	cout << "*** Welcome to Simpletron! ***" << endl << "*** "
+			<< "Please enter your program one instruction" << " ***" << endl
+			<< "*** " << "(or data word) at a time. I will type the" << " ***"
+			<< endl << "*** " << "location number and a question mark (?)."
+			<< " ***" << endl << "*** "
+			<< "You then type the word for that location." << " ***" << endl
+			<< "*** " << "Type the sentinel -99999 to stop entering" << " ***"
+			<< endl << "*** " << "your program." << " ***" << endl << endl; // prints !!!Hello World!!!
+
+	// Read in instructions
+	while (instructionCounter < MEMSIZE) {
+		cout << setfill('0') << setw(2) << instructionCounter << " ? ";
+		cin >> action;
+		cout << endl;
+		if (action == ENDLOOP)
+			break;
+		memory[instructionCounter] = action;
+		instructionCounter++;
+	}
+
+	cout << setfill(' ') << "*** Program loading completed ***" << endl;
+
+}
+
+void Simpletron::execute() {
+	cout << "*** Program execution begins ***" << endl;
+
+	instructionCounter = 0;
+// Execute typed program.
+	while (instructionCounter < MEMSIZE) {
+		instructionRegister = memory[instructionCounter];
+		operationCode = instructionRegister / 100;
+		operand = instructionRegister % 100;
+		switch (operationCode) {
+
+		// Essentially the part of the Simpletron that interprets the SML programming language.
+		case READ:
+			cout << "?";
+			cin >> memory[operand];
+			cout << endl;
+			break;
+		case LOAD:
+			accumulator = memory[operand];
+			break;
+		case ADD:
+			accumulator += memory[operand];
+			break;
+		case BRANCH:
+			instructionCounter = operand;
+			--instructionCounter;
+			break;
+		case BRANCHNEG:
+			if (accumulator < 0) {
+				instructionCounter = operand;
+				--instructionCounter;
+			}
+			break;
+		case BRANCHZERO:
+			if (accumulator == 0) {
+				instructionCounter = operand;
+				--instructionCounter;
+			}
+			break;
+		case HALT:
+			cout << endl << "*** Simpletron execution terminated ***" << endl;
+
+			cout << "REGISTERS:" << endl;
+
+			// List registers.
+			const int REGWIDTH = 20;
+			cout << setw(REGWIDTH) << left << "accumulator" << showpos
+					<< setw(5) << setfill('0') << internal << accumulator
+					<< endl << setfill(' ') << setw(REGWIDTH + 3) << left
+					<< "instructionCounter" << noshowpos << setw(2)
+					<< setfill('0') << instructionCounter << endl
+					<< setfill(' ') << setw(REGWIDTH) << left
+					<< "instructionRegister" << showpos << setw(5)
+					<< setfill('0') << internal << instructionRegister << endl
+					<< setfill(' ') << setw(REGWIDTH + 3) << left
+					<< "operationCode" << noshowpos << setw(2) << setfill('0')
+					<< operationCode << endl << setfill(' ')
+					<< setw(REGWIDTH + 3) << left << "operand" << setw(2)
+					<< setfill('0') << operand << endl << endl << setfill(' ')
+					<< "MEMORY:" << endl << "  ";
+
+			// print column names for memory dump.
+			for (int i = 0; i < 10; i++) {
+				cout << " " << setw(5) << right << i;
+			}
+
+			cout << endl;
+
+			for (int i = 0; i < MEMSIZE; i += 10) {
+				//Generate tens row list.
+				cout << setfill(' ') << setw(2) << i << showpos;
+				for (int j = 0; j < 10; j++) {
+					cout << " " << setw(5) << setfill('0') << internal
+							<< memory[i + j];
+				}
+				cout << noshowpos << endl;
+			}
+
+			break;
+		}
+
+		++instructionCounter;
+	}
 }
 
--- Simpletron.h
+++ Simpletron.h
@@ -12,27 +12,38 @@ class Simpletron {
 public:
 	Simpletron();
 	void printState();
+	void inputInstructions();
+	void execute();
 private:
-	const int MEMSIZE = 100;
-	const int ENDLOOP = -99999;
+	static const int MEMSIZE = 100;
+	static const int ENDLOOP = -99999;
 
 	//Set operation code constants.
-	const int READ = 10;
-	const int WRITE = 11;
+	static const int READ = 10;
+	static const int WRITE = 11;
 
-	const int LOAD = 20;
-	const int STORE = 21;
+	static const int LOAD = 20;
+	static const int STORE = 21;
 
-	const int ADD = 30;
-	const int SUBTRACT = 31;
+	static const int ADD = 30;
+	static const int SUBTRACT = 31;
 
-	const int DIVIDE = 32;
-	const int MULTIPLY = 33;
+	static const int DIVIDE = 32;
+	static const int MULTIPLY = 33;
 
-	const int BRANCH = 40;
-	const int BRANCHNEG = 41;
-	const int BRANCHZERO = 42;
-	const int HALT = 43;
+	static const int BRANCH = 40;
+	static const int BRANCHNEG = 41;
+	static const int BRANCHZERO = 42;
+	static const int HALT = 43;
+
+	//Initialise special registers.
+	int instructionRegister, operationCode, operand;
+	int action, accumulator, instructionCounter;
+
+	// Array to store program inside (Essentially RAM)
+	int memory[MEMSIZE];
+
+
 };
 
 #endif /* SIMPLETRON_H_ */
